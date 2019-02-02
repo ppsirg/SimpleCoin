5fdc2cef9dc0e679073dfd21b48f7cf25169c532
pedro rivera <ppsirg@gmail.com>
2019-02-02 07:00:27 -0500

    changes on keys managment

--- simpleCoin/miner_config.py
+++ simpleCoin/miner_config.py
@@ -1,9 +1,7 @@
 """Configure this file before you start mining. Check wallet.py for
 more details.
 """
-
-# Write your generated adress here. All coins mined will go to this address
-MINER_ADDRESS = "YFJ3BMkttcXMeLTIsLO1BuK4mSG9I4LwlbqS70lf5NksP1cejV0suqDHpwp8K8QFi6955iPkmXOUWw3I0bGRPg=="
+import json
 
 # Write your node url or ip. If you are running it localhost use default
 MINER_NODE_URL = "http://localhost:5000"
@@ -11,3 +9,11 @@ MINER_NODE_URL = "http://localhost:5000"
 # Store the url data of every other node in the network
 # so that we can communicate with them
 PEER_NODES = []
+
+# Write your generated adress here. All coins mined will go to this address
+MINER_KEY = 'lol'
+
+with open(MINER_KEY + '.key', 'r') as f:
+    key_data = json.load(f)
+
+MINER_ADDRESS = key_data['public_key']
--- simpleCoin/wallet.py
+++ simpleCoin/wallet.py
@@ -22,6 +22,8 @@ import requests
 import time
 import base64
 import ecdsa
+from threading import Thread
+import json
 
 from infrastructure import base_url
 
@@ -35,23 +37,75 @@ def wallet():
         3. Check transactions\n""")
     if response == "1":
         # Generate new wallet
-        print("""=========================================\n
-IMPORTANT: save this credentials or you won't be able to recover your wallet\n
-=========================================\n""")
         generate_ECDSA_keys()
     elif response == "2":
-        addr_from = input("From: introduce your wallet address (public key)\n")
-        private_key = input("Introduce your private key\n")
-        addr_to = input("To: introduce destination wallet address\n")
-        amount = input("Amount: number stating how much do you want to send\n")
-        print("=========================================\n\n")
-        print("Is everything correct?\n")
-        print("From: {0}\nPrivate Key: {1}\nTo: {2}\nAmount: {3}\n".format(addr_from, private_key, addr_to, amount))
-        response = input("y/n\n")
-        if response.lower() == "y":
-            send_transaction(addr_from, private_key, addr_to, amount)
+        send_transaction_ui()
     else:  # Will always occur when response == 3.
         check_transactions()
+
+# =============================================================================
+# graphic interface
+
+def generate_ECDSA_keys():
+    """This function takes care of creating your private and public (your address) keys.
+    It's very important you don't lose any of them or those wallets will be lost
+    forever. If someone else get access to your private key, you risk losing your coins.
+
+    private_key: str
+    public_ley: base64 (to make it shorter)
+    """
+    filename = input("Write the name of your new address: ") + ".key"
+    while True:
+        sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1) #this is your sign (private key)
+        private_key = sk.to_string().hex() #convert your private key to hex
+        vk = sk.get_verifying_key() #this is your verification key (public key)
+        public_key = vk.to_string().hex()
+        #we are going to encode the public key to make it shorter
+        public_key = base64.b64encode(bytes.fromhex(public_key))
+        if allowed_characters(public_key):
+            break
+
+    with open(filename, "w") as f:
+        key_data = {
+            'private_key': private_key,
+            'public_key': public_key.decode()
+        }
+        json.dump(key_data, f, sort_keys=True, indent=4)
+        print("Private key: {0}\nWallet address / Public key: {1}".format(private_key, public_key.decode()))
+    print("Your new address and private key are now in the file {0}".format(filename))
+
+def send_transaction_ui():
+    """Send transaction user interface.
+    """
+    key_filename = input("From: introduce your keys name\n")
+    with open(key_filename + '.key', 'r') as f:
+        key_data = json.load(f)
+    addr_from = key_data['public_key']
+    private_key = key_data['private_key']
+    print('your key {} was loaded'.format(key_filename))
+    print(key_data)
+    addr_to = input("\nTo: introduce destination wallet address\n")
+    amount = input("Amount: number stating how much do you want to send\n")
+    print("=========================================\n\n")
+    print("Is everything correct?\n")
+    print("From: {0}\nPrivate Key: {1}\nTo: {2}\nAmount: {3}\n".format(addr_from, private_key, addr_to, amount))
+    response = input("y/n\n")
+    if response.lower() == "y":
+        send_transaction(addr_from, private_key, addr_to, amount)
+
+# =============================================================================
+# auxiliar function
+
+def allowed_characters(key):
+    banned = [b'+', b'/']
+    for i in banned:
+        if i in key:
+            return False
+    return True
+
+
+# =============================================================================
+# transactions
 
 
 def send_transaction(addr_from, private_key, addr_to, amount):
@@ -61,12 +115,7 @@ def send_transaction(addr_from, private_key, addr_to, amount):
     having a longer chain. So make sure your transaction is deep into the chain
     before claiming it as approved!
     """
-    # For fast debugging REMOVE LATER
-    # private_key="181f2448fa4636315032e15bb9cbc3053e10ed062ab0b2680a37cd8cb51f53f2"
-    # amount="3000"
-    # addr_from="SD5IZAuFixM3PTmkm5ShvLm1tbDNOmVlG7tg6F5r7VHxPNWkNKbzZfa+JdKmfBAIhWs9UKnQLOOL1U+R3WxcsQ=="
-    # addr_to="SD5IZAuFixM3PTmkm5ShvLm1tbDNOmVlG7tg6F5r7VHxPNWkNKbzZfa+JdKmfBAIhWs9UKnQLOOL1U+R3WxcsQ=="
-
+    print(len(private_key), private_key)
     if len(private_key) == 64:
         signature, message = sign_ECDSA_msg(private_key)
         url = base_url + '/txion'
@@ -83,43 +132,6 @@ def send_transaction(addr_from, private_key, addr_to, amount):
         print("Wrong address or key length! Verify and try again.")
 
 
-def check_transactions():
-    """Retrieve the entire blockchain. With this you can check your
-    wallets balance. If the blockchain is to long, it may take some time to load.
-    """
-    res = requests.get(base_url + '/blocks')
-    print(res.text)
-
-def allowed_characters(key):
-    banned = [b'+', b'/']
-    for i in banned:
-        if i in key:
-            return False
-    return True
-
-def generate_ECDSA_keys():
-    """This function takes care of creating your private and public (your address) keys.
-    It's very important you don't lose any of them or those wallets will be lost
-    forever. If someone else get access to your private key, you risk losing your coins.
-
-    private_key: str
-    public_ley: base64 (to make it shorter)
-    """
-    while True:
-        sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1) #this is your sign (private key)
-        private_key = sk.to_string().hex() #convert your private key to hex
-        vk = sk.get_verifying_key() #this is your verification key (public key)
-        public_key = vk.to_string().hex()
-        #we are going to encode the public key to make it shorter
-        public_key = base64.b64encode(bytes.fromhex(public_key))
-        if allowed_characters(public_key):
-            break
-
-    filename = input("Write the name of your new address: ") + ".txt"
-    with open(filename, "w") as f:
-        f.write("Private key: {0}\nWallet address / Public key: {1}".format(private_key, public_key.decode()))
-    print("Your new address and private key are now in the file {0}".format(filename))
-
 def sign_ECDSA_msg(private_key):
     """Sign the message to be sent
     private_key: must be hex
@@ -134,6 +146,14 @@ def sign_ECDSA_msg(private_key):
     sk = ecdsa.SigningKey.from_string(bytes.fromhex(private_key), curve=ecdsa.SECP256k1)
     signature = base64.b64encode(sk.sign(bmessage))
     return signature, message
+
+
+def check_transactions():
+    """Retrieve the entire blockchain. With this you can check your
+    wallets balance. If the blockchain is to long, it may take some time to load.
+    """
+    res = requests.get(base_url + '/blocks')
+    print(res.text)
 
 
 if __name__ == '__main__':
